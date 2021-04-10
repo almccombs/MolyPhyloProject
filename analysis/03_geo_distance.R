f8910842f6c99446330b4968c08e8c0a81644ad0
almccombs <amccombs@iastate.edu>
2021-04-10 14:21:39 -0500

    calculate geographic distances

--- analysis/03_geo_distance.R
+++ analysis/03_geo_distance.R
@@ -4,3 +4,5 @@ load("data/pop_data_n146.Rdata")
 sites <- mydat[match(unique(mydat$SiteID), mydat$SiteID),]
 
 site_dist <- geodist(x = sites, sequential = F)
+
+save(site_dist, file = "output/site_geodistance_matrix.Rdata")
